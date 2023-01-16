04762536ae89b6e3a9f87b4405e0ceb17c5ef4f1
32Fire32 <nicola.gianfelici@hotmail.it>
2023-01-16 15:00:57 +0100

    inserisce i dati all'interno delle card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,19 @@
 @extends('layout.main')
 
 @section('main-content')
-    <h1>Main Content</h1>
-    {{-- @dd($movies) --}}
-    @foreach ($movies as $movie)
-        <p>{{ $movie->title }}</p>
-    @endforeach
+    <div class="container text-center">
+        <h1>Main Content</h1>
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="card col-md-4 p-2">
+                    <h4>{{ $movie->title }}</h4>
+                    <h5>Titolo originale: {{ $movie->title }} </h5>
+                    <span>Nazionalità: {{ $movie->nationality }}</span>
+                    <span>Del: {{ $movie->date }} </span>
+                    <span>Voto: {{ $movie->vote }}</span>
+                </div>
+            @endforeach
+
+        </div>
+    </div>
 @endsection
